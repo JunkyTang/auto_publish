8191823d77d0d3dff51b5cb67d7dabdf115d6962
汤俊杰 <921969987@qq.com>
2024-11-23 18:06:50 +0800

    0.1.3

--- setup.py
+++ setup.py
@@ -2,18 +2,37 @@ from setuptools import setup, find_packages
 
 setup(
     name="Lucky",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "requests",
-        "git+https://github.com/JunkyTang/auto_publish.git@0.1.2#egg=Lucky",
     ],
+    author="Lucky",
+    author_email="Luck@where.com",  # 作者邮箱
+    description="A short description of your package",  # 包的简短描述
+    long_description="A long description of your package",  # 包的详细描述
+    long_description_content_type="README.md",  # 描述格式，支持 reStructuredText、Markdown 等
+    url="https://github.com/JunkyTang/auto_publish",  # 包的主页或 GitHub 地址
+    classifiers=[  # 用于分类包的类别标签（用于 PyPI）
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',  # 指定支持的 Python 版本
     entry_points={
         "console_scripts": [
             "lucky-build=Lucky.builder:main",
             "lucky-upload=Lucky.uploader:main"
         ]
     },
+    # include_package_data=True,  # 如果你有额外的文件（如图片、配置文件等）需要包含在包内
+    # package_data={  # 通过包的目录结构来指定额外的文件
+    #     'your_package': ['data/*.dat'],
+    # },
+    # zip_safe=False,  # 如果你的包是纯 Python 包，可以设置为 True
+
+
 )
 
 
